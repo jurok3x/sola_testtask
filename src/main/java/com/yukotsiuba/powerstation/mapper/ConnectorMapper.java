fcbcc95eddde9c225b3ce38f8f856e620e15ffd5
ykotsiuba <jurok3x@gmail.com>
2024-02-26 17:19:15 +0200

    Added mappers.

---
+++ src/main/java/com/yukotsiuba/powerstation/mapper/ConnectorMapper.java
@@ -0,0 +1,23 @@
+package com.yukotsiuba.powerstation.mapper;
+
+import com.yukotsiuba.powerstation.dto.ConnectorRequestDTO;
+import com.yukotsiuba.powerstation.dto.ConnectorResponseDTO;
+import com.yukotsiuba.powerstation.entity.Connector;
+
+public class ConnectorMapper {
+    public static Connector toEntity(ConnectorRequestDTO request) {
+        Connector connector = new Connector();
+        connector.setType(request.getType());
+        connector.setMaxPower(request.getMaxPower());
+        return connector;
+    }
+
+    public static ConnectorResponseDTO toResponseDTO(Connector connector) {
+        return ConnectorResponseDTO
+                .builder()
+                .id(connector.getId())
+                .type(connector.getType())
+                .maxPower(connector.getMaxPower())
+                .build();
+    }
+}
