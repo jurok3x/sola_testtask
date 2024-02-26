12bf204ce7dd4c9eb0a81c8a4b62a020b990c288
ykotsiuba <jurok3x@gmail.com>
2024-02-26 09:56:52 +0200

    First commit

---
+++ src/main/java/com/yukotsiuba/powerstation/PowerStationApplication.java
@@ -0,0 +1,13 @@
+package com.yukotsiuba.powerstation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerStationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerStationApplication.class, args);
+	}
+
+}
