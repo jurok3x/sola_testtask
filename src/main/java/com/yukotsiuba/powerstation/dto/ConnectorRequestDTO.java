1ca0649e86b0fe7412cf0ad2f2549ffcfb83bbf1
ykotsiuba <jurok3x@gmail.com>
2024-02-27 16:06:07 +0200

    Moved constraints to dto layer.

--- src/main/java/com/yukotsiuba/powerstation/dto/ConnectorRequestDTO.java
+++ src/main/java/com/yukotsiuba/powerstation/dto/ConnectorRequestDTO.java
@@ -1,11 +1,19 @@
 package com.yukotsiuba.powerstation.dto;
 
+import com.yukotsiuba.powerstation.entity.annotations.ValidEnum;
 import com.yukotsiuba.powerstation.entity.enums.ConnectorType;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 
 @Data
 public class ConnectorRequestDTO {
-    private ConnectorType type;
 
+    @NotNull(message = "Type can not be null.")
+    @ValidEnum(enumClass = ConnectorType.class)
+    private String type;
+
+    @NotNull(message = "Maximum power can not be null.")
+    @Positive(message = "Invalid maximum power value.")
     private Integer maxPower;
 }
--- src/main/java/com/yukotsiuba/powerstation/entity/Connector.java
+++ src/main/java/com/yukotsiuba/powerstation/entity/Connector.java
@@ -3,8 +3,6 @@ package com.yukotsiuba.powerstation.entity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.yukotsiuba.powerstation.entity.enums.ConnectorType;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import lombok.Data;
 
 import java.util.UUID;
@@ -24,11 +22,8 @@ public class Connector {
 
     @Column(name = "type")
     @Enumerated(EnumType.STRING)
-    @NotNull(message = "Type can not be null.")
     private ConnectorType type;
 
     @Column(name = "max_power", columnDefinition = "INTEGER CHECK (max_power > 0)")
-    @NotNull
-    @Positive(message = "Invalid maximum power value.")
     private Integer maxPower;
 }
