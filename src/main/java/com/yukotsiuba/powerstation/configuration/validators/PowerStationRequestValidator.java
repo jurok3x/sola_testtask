d3be12ca59996c5c0be7621679b2355f817851c3
ykotsiuba <jurok3x@gmail.com>
2024-02-27 16:06:51 +0200

    Fixed error handling to show all errors.

--- src/main/java/com/yukotsiuba/powerstation/configuration/validators/PowerStationRequestValidator.java
+++ src/main/java/com/yukotsiuba/powerstation/configuration/validators/PowerStationRequestValidator.java
@@ -1,16 +1,18 @@
 package com.yukotsiuba.powerstation.configuration.validators;
 
-import com.yukotsiuba.powerstation.entity.PowerStation;
+import com.yukotsiuba.powerstation.dto.PowerStationRequestDTO;
 import com.yukotsiuba.powerstation.entity.annotations.PowerStationValidator;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.yukotsiuba.powerstation.utils.ValidationUtils.capitalize;
 import static com.yukotsiuba.powerstation.utils.ValidationUtils.isEmpty;
 
-public class PowerStationRequestValidator implements ConstraintValidator<PowerStationValidator, PowerStation> {
+public class PowerStationRequestValidator implements ConstraintValidator<PowerStationValidator, PowerStationRequestDTO> {
 
     private String[] conditionallyMandatoryFields;
 
@@ -20,7 +22,7 @@ public class PowerStationRequestValidator implements ConstraintValidator<PowerSt
     }
 
     @Override
-    public boolean isValid(PowerStation value, ConstraintValidatorContext context) {
+    public boolean isValid(PowerStationRequestDTO value, ConstraintValidatorContext context) {
         if (value == null) {
             return true;
         }
@@ -28,19 +30,23 @@ public class PowerStationRequestValidator implements ConstraintValidator<PowerSt
         boolean isPublic = Boolean.TRUE.equals(value.getIsPublic());
 
         if (isPublic) {
+            List<String> errorFields = new ArrayList<>();
             for (String field : conditionallyMandatoryFields) {
                 if (isEmpty(getFieldValue(value, field))) {
-                    addConstraintViolation(context, capitalize(field) + " is mandatory when 'isPublic' is true.", field);
-                    return false;
+                    errorFields.add(field);
                 }
+            }
+            if(!errorFields.isEmpty()) {
+                addConstraintViolations(context, errorFields);
+                return false;
             }
         }
         return true;
     }
 
-    private Object getFieldValue(PowerStation station, String fieldName) {
+    private Object getFieldValue(PowerStationRequestDTO station, String fieldName) {
         try {
-            Field field = PowerStation.class.getDeclaredField(fieldName);
+            Field field = PowerStationRequestDTO.class.getDeclaredField(fieldName);
             field.setAccessible(true);
             return field.get(station);
         } catch (NoSuchFieldException | IllegalAccessException e) {
@@ -48,10 +54,13 @@ public class PowerStationRequestValidator implements ConstraintValidator<PowerSt
         }
     }
 
-    private void addConstraintViolation(ConstraintValidatorContext context, String message, String fieldName) {
-        context.disableDefaultConstraintViolation();
-        context.buildConstraintViolationWithTemplate(message)
-                .addPropertyNode(fieldName)
-                .addConstraintViolation();
+    private void addConstraintViolations(ConstraintValidatorContext context, List<String> errorFields) {
+        for(String field: errorFields) {
+            String message = String.format("%s is mandatory for public stations.", capitalize(field));
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(message)
+                    .addPropertyNode(field)
+                    .addConstraintViolation();
+        }
     }
 }
