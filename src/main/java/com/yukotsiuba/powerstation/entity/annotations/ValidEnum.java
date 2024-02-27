244039a3ecb91cfe596539aa827f3e46c2e7543c
ykotsiuba <jurok3x@gmail.com>
2024-02-27 16:04:57 +0200

    Added enums validator.

---
+++ src/main/java/com/yukotsiuba/powerstation/entity/annotations/ValidEnum.java
@@ -0,0 +1,24 @@
+package com.yukotsiuba.powerstation.entity.annotations;
+
+import com.yukotsiuba.powerstation.configuration.validators.EnumValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = EnumValidator.class)
+public @interface ValidEnum {
+
+    Class<? extends Enum<?>> enumClass();
+
+    String message() default "Invalid enum value";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
